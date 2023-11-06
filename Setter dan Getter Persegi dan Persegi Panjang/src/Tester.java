9660e4fdb44153021bf4d7964cddbac569d3b902
Maulana Ikhsan <anjay1435@gmail.com>
2023-11-07 01:16:07 +0700

    Update Pada Module Setter dan Getter pada Persegi Panjang dan persegi

--- Setter dan Getter Persegi dan Persegi Panjang/src/Tester.java
+++ Setter dan Getter Persegi dan Persegi Panjang/src/Tester.java
@@ -2,8 +2,21 @@
 public class Tester {
     public static void main(String[] args) {
         Persegi persegi = new Persegi();
+        PersegiPanjang persegiPanjang = new PersegiPanjang();
 
         persegi.setSisi(10);
-        System.out.println(persegi.hitungKeliling());
+        System.out.println("Persegi");
+        System.out.println("Sisi " + persegi.getSisi());
+        System.out.println("Keliling " + persegi.hitungKeliling());
+        System.out.println("Luas " + persegi.hitungLuas() + "\n");
+
+        persegiPanjang.setPanjang(10);
+        persegiPanjang.setLebar(5);
+        System.out.println("Persegi Panjang");
+        System.out.println("Panjang " + persegiPanjang.getPanjang());
+        System.out.println("Lebar " + persegiPanjang.getLebar());
+        System.out.println("Keliling " + persegiPanjang.hitungKeliling());
+        System.out.println("Luas " + persegiPanjang.luas());
+
     }
 }
